5cfa3775d58bb7647ed61983a3c140a05322516e
Daniel R <nzdanron@gmail.com>
2025-03-27 20:40:37 +1300

    feat: customise bar
    Probably need to upgrade to json at some point

--- scripts/bar-status.sh
+++ scripts/bar-status.sh
@@ -1,10 +1,54 @@
 #!/usr/bin/env bash
 
+RED='\033[0;31m'
+NC='\033[0m' # No Color
+
+red() {
+    printf "${RED}$@${NC}\n"
+}
+
+update_brightness() {
+    local current_brightness=$(brightnessctl g)
+    local max_brightness=$(brightnessctl m)
+    local brightness_percentage=$(( current_brightness * 100 / max_brightness ))
+
+    BRIGHTNESS="$brightness_percentage%"
+}
+
+update_time() {
+    TIME=$(date +'%Y-%m-%d %H:%M')
+}
+
+update_volume() {
+    VOLUME=$(pactl get-sink-volume @DEFAULT_SINK@ | awk '{print $5}')
+}
+
+update_battery() {
+    local battery_text=$(acpi | awk '{print $4}' | sed 's/%,//')
+
+    local charging= $(acpi | awk '{print $3}' | sed "s/,//")
+
+    # FIXME: Not working
+    if [ "$charging" == "Charging" ]; then
+        battery_text="$battery_text+"
+    fi
+
+    BATTERY="$battery_text%"
+}
+
+print_bar() {
+    update_time
+    update_brightness
+    update_volume
+    update_battery
+
+    echo -n "[ BAT: $BATTERY ] [ VOL: $VOLUME ] [ BRT: $BRIGHTNESS ] [ $TIME ] "
+}
+
 main() {
     while true; do
-        local time=$(date +'%Y-%m-%d %X')
-        echo "| $time |"
-        sleep 1
+        print_bar
+        sleep 5
     done
 }
 
