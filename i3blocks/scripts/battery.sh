94293492d5b1e1ad9f3b5f3e933c9355f783e18b
Daniel R <nzdanron@gmail.com>
2025-04-01 16:40:38 +1300

    feat: update battery script to remove acpi dependency

--- i3blocks/scripts/battery.sh
+++ i3blocks/scripts/battery.sh
@@ -2,20 +2,17 @@
 #
 # Script for outputting the current battery percentage
 
+BATTERY="/sys/class/power_supply/BAT0"
+
 main() {
-    local battery_text=$(acpi | awk '{print $4}' | sed 's/%,//')
+    local battery_percentage=$(cat "$BATTERY/capacity")
 
-    # Covering edge case of laptop charged fully
-    if [ "$battery_text" == "charging," ]; then
-        battery_text=100
-    fi
-
-    local charging=$(acpi | awk '{print $3}' | sed "s/,//")
+    local charging=$(cat "$BATTERY/status")
     if [ "$charging" == "Charging" ]; then
         charging_text=" CHARGING"
     fi
 
-    echo "[ BAT: $battery_text%$charging_text ]"
+    echo "[ BAT: $battery_percentage%$charging_text ]"
 }
 
 main
