fd7d75bfccb8436842ba0bde2d89260ff056f872
Daniel R <nzdanron@gmail.com>
2025-04-14 23:33:09 +1200

    feat: click on volume block opens pavucontrol

--- i3blocks/scripts/volume.sh
+++ i3blocks/scripts/volume.sh
@@ -1,11 +1,16 @@
 #!/usr/bin/env bash
 #
-# Script for outputting the current volume
+# Script for the volume block
 
 main() {
+    # If left button is clicked, then BLOCK_BUTTON will be set to 1
+    if [[ $BLOCK_BUTTON == 1 ]]; then
+        pavucontrol &> /dev/null &
+    fi
+
     local volume=$(pactl get-sink-volume @DEFAULT_SINK@ | awk '{print $5}')
 
     echo "[ VOL: $volume ]"
 }
 
-main
+main "$@"
